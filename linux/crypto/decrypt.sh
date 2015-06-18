96e70cc7e6beaa765374dbb026ca3735202c62cb
Raghav sidhanti <sudohippie@users.noreply.github.com>
2015-06-18 11:10:23 -0700

    More colors :)

--- linux/crypto/decrypt.sh
+++ linux/crypto/decrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-RED_COL="\033[0;31m"
-GREEN_COL="\033[0;32m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
 NO_COL="\033[0m"
 
 # check arguments
@@ -9,14 +9,14 @@ if [ "$#" -eq 1 ]
 then
     INFILE=$1
 else
-    echo "${RED_COL}Invalid arguments. Missing input argument.${NO_COL}"
+    echo "${RED}Invalid arguments. Missing input argument.${NO_COL}"
     exit 1;
 fi
 
 # check input file
 if [ ! -e $INFILE ]
 then
-    echo "${RED_COL}Invid argument. Input file does not exist.${NO_COL}"
+    echo "${RED}Invid argument. Input file does not exist.${NO_COL}"
     exit 1;
 fi
 
@@ -24,9 +24,9 @@ openssl enc -d -a -aes-256-cbc -in $INFILE | tar zxvf -
 
 if [ $? -eq 0 ]
 then
-    echo "${GREEN_COL}Decryption successful: $INFILE${NO_COL}"
+    echo "${GREEN}Decryption successful: $INFILE${NO_COL}"
 else
-    echo "${RED_COL}Decryption failed.${NO_COL}"
+    echo "${RED}Decryption failed.${NO_COL}"
 fi
 
 exit 0
--- linux/crypto/encrypt.sh
+++ linux/crypto/encrypt.sh
@@ -2,9 +2,9 @@
 
 curr_time=$(date +%Y%m%d%H%M%S)
 
-RED_COL="\033[0;31m"
-GREEN_COL="\033[0;32m"
-YELLOL_COL="\033[0;33m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
 NO_COL="\033[0m"
 
 # check arguments
@@ -18,20 +18,20 @@ then
     INFILE="$1"
     OUTFILE="$2"
 else
-    >&2 echo "${RED_COL}Invalid number of arguments. Please specify one or two argument(s) only.${NO_COL}"
+    >&2 echo "${RED}Invalid number of arguments. Please specify one or two argument(s) only.${NO_COL}"
     exit 1;
 fi
 
 # check input and output files
 if [ ! -e $INFILE ]
 then
-    >&2 echo "${RED_COL}Invalid argument. Input file does not exist.${NO_COL}"
+    >&2 echo "${RED}Invalid argument. Input file does not exist.${NO_COL}"
     exit 1;
 fi
 
 if [ -e $OUTFILE ]
 then
-    >&2 echo "${RED_COL}Invalid argument. Output file already exists and will not be overwritten.${NO_COL}"
+    >&2 echo "${RED}Invalid argument. Output file already exists and will not be overwritten.${NO_COL}"
     exit 1;
 fi
 
@@ -40,9 +40,9 @@ tar zcvf - $INFILE | openssl enc -e -a -aes-256-cbc -out $OUTFILE
 
 if [ $? -eq 0 ]
 then
-    echo "${GREEN_COL}Encryption successful: ${YELLOL_COL}$INFILE -> $OUTFILE${NO_COL}"
+    echo "${GREEN}Encryption successful: ${YELLOw_COL}$INFILE -> $OUTFILE${NO_COL}"
 else
-    echo "${RED_COL}Encryption failed.${NO_COL}"
+    echo "${RED}Encryption failed.${NO_COL}"
 fi
 
 exit 0;
