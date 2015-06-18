4146982f4f100c767b0ada6d38d0af0ed073da18
Raghav sidhanti <sudohippie@users.noreply.github.com>
2015-06-17 21:00:45 -0700

    Decryption script

--- linux/crypto/decrypt.sh
+++ linux/crypto/decrypt.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-openssl enc -d -a -aes-256-cbc -in $1 | tar zxvf -
+
+# check arguments
+if [ "$#" -eq 1 ]
+then
+    INFILE=$1
+else
+    echo "Invalid arguments. Missing input argument."
+    exit 1;
+fi
+
+# check input file
+if [ ! -e $INFILE ]
+then
+    echo "Invid argument. Input file does not exist."
+    exit 1;
+fi
+
+openssl enc -d -a -aes-256-cbc -in $INFILE | tar zxvf -
+
+echo "Decrypted: $INFILE"
+exit 0
